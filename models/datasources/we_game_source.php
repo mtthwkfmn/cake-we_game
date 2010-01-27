21f4693d25de5a9747251394a44b8e2b225321df
Miles Johnson <mileswjohnson@gmail.com>
2010-01-26 23:40:21 -0800

    Uploading the datasource to GitHub,

---
+++ models/datasources/we_game_source.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * WeGame DataSource
+ *
+ * A datasource that hooks up to the WeGame API and fetches XML data using an HTTP Request.
+ *
+ * @author 		Miles Johnson - www.milesj.me
+ * @copyright	Copyright 2006-2009, Miles Johnson, Inc.
+ * @license 	http://www.opensource.org/licenses/mit-license.php - Licensed under The MIT License
+ * @link		www.milesj.me/resources/script/wegame-datasource
+ * @link		www.wegame.com, api.wegame.com
+ */
+
+App::import('Core', array('HttpSocket', 'Xml'));
+
+class WeGameSource extends DataSource {
+
+	/**
+	 * Current version: www.milesj.me/resources/script/wegame-datasource
+	 *
+	 * @access public
+	 * @var string
+	 */
+	public $version = '0.8';
+
+	/**
+	 * The URL for the API.
+	 *
+	 * @var string
+	 */
+	const API_URL = 'http://api.wegame.com';
+
+	/**
+	 * Set the cache settings.
+	 *
+	 * @access public
+	 * @param array $config
+	 * @return void
+	 */
+	public function __construct($config = array()) {
+		parent::__construct($config);
+
+		if (Cache::config('weGame') === false) {
+			Cache::config('weGame', array(
+				'engine' 	=> 'File',
+				'serialize' => true,
+				'prefix'	=> '',
+				'path' 		=> CACHE .'we_game'. DS,
+				'duration'	=> '+1 day'
+			));
+		}
+	}
+
+	/**
+	 * Describe the data source. Nothing really to put here.
+	 *
+	 * @access public
+	 * @param object $Model
+	 * @return string
+	 */
+	public function describe($Model) {
+		return self::API_URL;
+	}
+
+	/**
+	 * List out all the sources, aka the API URL.
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function listSource() {
+		return self::API_URL;
+	}
+
+	/**
+	 * Read / fetch data from the API and format the response before returning.
+	 *
+	 * @access public
+	 * @param string $Model
+	 * @param array $query
+	 * @return array
+	 */
+	public function read($Model, array $query = array()) {
+		$cache = true;
+		$cacheTime = '+1 day';
+		$cacheKey = md5(serialize($query['conditions']));
+
+		if (isset($query['conditions']['url'])) {
+			$url = $query['conditions']['url'];
+			unset($query['conditions']['url']);
+		} else {
+			return null;
+		}
+
+		if (isset($query['conditions']['id'])) {
+			$url .= $query['conditions']['id'] .'/';
+			unset($query['conditions']['id']);
+		}
+
+		if (isset($query['conditions']['cache'])) {
+			if (is_bool($query['conditions']['cache'])) {
+				$cache = $query['conditions']['cache'];
+			} else if (is_string($query['conditions']['cache'])) {
+				$cacheTime = $query['conditions']['cache'];
+			}
+			unset($query['conditions']['cache']);
+		}
+
+		// Find cached first
+		if ($cache === true) {
+			Cache::set(array('duration' => $cacheTime));
+			$results = Cache::read($cacheKey, 'weGame');
+
+			if (is_array($results)) {
+				return $results;
+			}
+		}
+
+		$this->Http = new HttpSocket();
+		$response = $this->Http->get(self::API_URL . $url, array_merge($query['conditions'], array('api_key' => $this->config['apiKey'])));
+
+		if (substr($response, 0, 5) == '<?xml') {
+			$xml = new Xml($response);
+			$xml = $xml->toArray();
+
+			if ($cache === true) {
+				Cache::set(array('duration' => $cacheTime));
+				Cache::write($cacheKey, $xml, 'weGame');
+			}
+
+			return $xml;
+		} else {
+			return $response;
+		}
+	}
+
+}
---
+++ models/we_game.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * WeGame Model
+ *
+ * Base model that augments the WeGame datasource and defines all the methods available for the API.
+ * Refer to the official API for documented parameters: http://api.wegame.com/docs
+ *
+ * @author 		Miles Johnson - www.milesj.me
+ * @copyright	Copyright 2006-2009, Miles Johnson, Inc.
+ * @license 	http://www.opensource.org/licenses/mit-license.php - Licensed under The MIT License
+ * @link		www.milesj.me/resources/script/wegame-datasource
+ * @link		www.wegame.com, api.wegame.com
+ */
+
+class WeGame extends AppModel {
+
+	/**
+	 * No database table needed.
+	 *
+	 * @access public
+	 * @var boolean
+	 */
+	public $useTable = false;
+
+	/**
+	 * Use the weGame datasource.
+	 *
+	 * @access public
+	 * @var boolean
+	 */
+	public $useDbConfig = 'weGame';
+
+	/**
+	 * Fetch a single games data, based on id|slug|title.
+	 *
+	 * @access public
+	 * @param int|string $id
+	 * @return array
+	 */
+	public function game($id) {
+		return $this->__request(array('url' => '/games/single/', 'id' => $id), array(), 'Game');
+	}
+
+	/**
+	 * Fetch all games within a certain timeframe.
+	 *
+	 * @access public
+	 * @param array $conditions
+	 * @return array
+	 */
+	public function games(array $conditions = array()) {
+		return $this->__request(array('url' => '/games/query/', 'created' => strtotime('-3 months')), $conditions, 'Game');
+	}
+
+	/**
+	 * Fetch all games that are supported on the client; within a certain timeframe.
+	 *
+	 * @access public
+	 * @param array $conditions
+	 * @return array
+	 */
+	public function supportedGames(array $conditions = array()) {
+		return $this->__request(array('url' => '/games/supported/', 'created' => strtotime('-3 months')), $conditions, 'Game');
+	}
+
+	/**
+	 * Fetch a single videos data, based on id|slug|title.
+	 *
+	 * @access public
+	 * @param int|string $id
+	 * @return array
+	 */
+	public function video($id) {
+		return $this->__request(array('url' => '/videos/single/', 'id' => $id), array(), 'Video');
+	}
+
+	/**
+	 * Fetch all videos based on multiple filtering criteria.
+	 *
+	 * @access public
+	 * @param array $conditions
+	 * @return array
+	 */
+	public function videos(array $conditions = array()) {
+		return $this->__request(array('url' => '/videos/query/', 'search' => '', 'offset' => '', 'sort' => 'views', 'tag' => '', 'created' => strtotime('-1 month')), $conditions, 'Video');
+	}
+
+	/**
+	 * Fetch a single screenshots data, based on id|slug|title.
+	 *
+	 * @access public
+	 * @param int|string $id
+	 * @return array
+	 */
+	public function screenshot($id) {
+		return $this->__request(array('url' => '/screenshots/single/', 'id' => $id), array(), 'Screenshot');
+	}
+
+	/**
+	 * Fetch all screenshots based on multiple filtering criteria.
+	 *
+	 * @access public
+	 * @param array $conditions
+	 * @return array
+	 */
+	public function screenshots(array $conditions = array()) {
+		return $this->__request(array('url' => '/screenshots/query/', 'search' => '', 'offset' => '', 'sort' => 'views', 'tag' => '', 'created' => strtotime('-1 month')), $conditions, 'Screenshot');
+	}
+
+	/**
+	 * Merge the default conditions with the user defined ones. Once prepared, dispatch the HTTP request to the API.
+	 * Will format and return the correct data, and or error string.
+	 *
+	 * @access private
+	 * @param array $defaults
+	 * @param array $conditions
+	 * @param string $type
+	 * @return string|array
+	 */
+	private function __request($defaults, $conditions, $type) {
+		$defaults = array_merge(array('cache' => true), $defaults);
+		$conditions = array_filter(array_intersect_key(array_merge($defaults, $conditions), $defaults));
+
+		if (isset($conditions['sort']) && !in_array($conditions['sort'], array('likes', 'comments', 'featured', 'views', 'date', 'game'))) {
+			$conditions['sort'] = 'views';
+		}
+
+		$results = $this->find('all', array('conditions' => $conditions));
+		
+		if (is_array($results)) {
+			if (isset($results['Results'][$type])) {
+				return $results['Results'][$type];
+
+			} else if (isset($results['Response']['Error'])) {
+				return $results['Response']['Error'];
+
+			} else {
+				return $results;
+			}
+		} else {
+			$results = (string)$results;
+
+			switch ($results) {
+				case 'INVALID_GAME':
+					$results = 'Invalid game was specified in a query or the game does not exist.';
+				break;
+				case 'DUPLICATE_GAMES_FOUND':
+					$results = 'The query matched multiple games.';
+				break;
+				case 'MISSING_API_KEY':
+					$results = 'You are trying to query a page that requires an API Key.';
+				break;
+				case 'INVALID_API_KEY':
+					$results = 'You are trying to query a page with an invalid API key.';
+				break;
+				default:
+					$results = 'An unexpected error has occurred. Please contact WeGame.com with the API issue.';
+				break;
+			}
+			
+			return $results;
+		}
+	}
+
+}
